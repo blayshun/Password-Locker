63ab5171002349a92103d9940885c7c7df92e513
Blayshun <34756140+blayshun@users.noreply.github.com>
2019-03-04 22:10:42 -0500

    Imported pyperclip to copy cmdline arg key-pair to clipboard; completed

--- pw.py
+++ pw.py
@@ -3,6 +3,7 @@
 # by guidlines of Automate the Boring Stuff with Python
 
 import sys
+import pyperclip
 
 
 PASSWORDS = {'email': '13dEnricocj*'}
@@ -12,3 +13,9 @@ if len(sys.argv) < 2:
     sys.exit()
 
 account = sys.argv[1]   # first command line argument is the account name
+
+if account in PASSWORDS:
+    pyperclip.copy(PASSWORDS[account])
+    print('Password for ' + account + ' copied to clipboard.')
+else:
+    print('There is no account named ' + account)
