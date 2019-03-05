f4e145fa8866cd736901867f61756629f3b9ac98
Blayshun <34756140+blayshun@users.noreply.github.com>
2019-03-04 21:26:54 -0500

    Added dictionary; added user guidance in case of error

--- pw.py
+++ pw.py
@@ -1,2 +1,14 @@
 #! python3
-# pw.py - A password locker program created by guidlines of Automate the Boring Stuff with Python
+# pw.py - A password locker program created
+# by guidlines of Automate the Boring Stuff with Python
+
+import sys
+
+
+PASSWORDS = {'email': '13dEnricocj*'}
+
+if len(sys.argv) < 2:
+    print('Usage: python pw.py [account] - copy account password')
+    sys.exit()
+
+account = sys.argv[1]   # first command line argument is the account name
